d2d71922c41b2de20581588635caeb0d06bab111
Anders S <asp@venzo.com>
2023-07-25 12:46:40 +0200

    GA PIM

--- src/functions.ps1
+++ src/functions.ps1
@@ -13,3 +13,30 @@ function Invoke-AzResourceGraphCheck {
     
     Search-AzGraph $Query | Select-Object -ExpandProperty Data | Where-Object { $_.compliant -eq 0 } | Select-Object -ExpandProperty id    
 }
+
+function Get-GroupMembers {
+    [CmdletBinding()]
+    param (
+        $DirectoryObjectByIds,
+        $DebugMode = $false
+    )
+    $params = @{
+        ids   = $DirectoryObjectByIds
+        types = @(
+            "user"
+            "group"
+        )
+    }
+
+    $DirectoryObject = Get-MgDirectoryObjectById -BodyParameter $params
+
+    $DirectoryObject | ForEach-Object {
+        $ObjectType = $_.AdditionalProperties.'@odata.type'
+        if ($ObjectType -eq '#microsoft.graph.group') {
+            Get-GroupMembers -DirectoryObjectByIds (Get-MgGroupMember -GroupId $_.Id | Select-Object -ExpandProperty Id)
+        }
+        if ($ObjectType -eq '#microsoft.graph.user') {
+            $_ 
+        }
+    }  
+}
